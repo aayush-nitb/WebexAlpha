4613e58612b6ed741b6a6de28814a915b77d9114
Aayush Agrawal <aayush.nitb@gmail.com>
2015-12-23 09:26:41 +0530

    auto compilation of coffee script and insertion of bower_components

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-
-/**
- * Module dependencies.
- */
+require('wiredep')({
+    src: 'views/layout.jade',
+    ignorePath: '../'
+});
 
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , fs = require('fs')
+  , walk = require('walk')
+  , coffee = require('coffee-script')
   , path = require('path');
+
+var controllers = walk.walk('./controllers', { followLinks: false });
+controllers.on('file', function (root, stat, next) {
+    var filename = path.join(root, stat.name);
+    var outfile = path.join("public/javascripts", stat.name + ".js");
+    fs.readFile(filename, 'utf8', function (err, data) {
+        var compiled = coffee.compile(data);
+        fs.writeFile(outfile, compiled, function(err){
+            if(!err) console.log("Compiled: " + filename + ", Output: " + outfile);
+        });
+    });
+    next();
+});
 
 var app = express();
 
@@ -20,7 +36,8 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+  app.use('/public', express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
